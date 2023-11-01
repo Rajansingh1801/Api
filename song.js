c33ca5276728f9d3a4d141a742b2f252c84eb7e9
Rajan singh <87349777+Rajansingh1801@users.noreply.github.com>
2023-11-01 11:48:50 +0530

    1

---
+++ song.js
@@ -0,0 +1,153 @@
+export const songs = [
+  {
+    movie_name: "Ramiya vastaiya",
+    img: "https://i.scdn.co/image/ab67616d0000b2739ca6c8941d918f92885acee8",
+    song_name: "Jine laga hoo",
+    singer_name: "Atif Aslam",
+    link: "https://youtu.be/VhRwuWp4MQ8",
+    time: "5:04",
+  },
+  {
+    img: "https://i.ytimg.com/vi/iHWE2HoyuIs/maxresdefault.jpg",
+    song_name: "Ve Maahi​",
+    singer_name: "Arijit Singh, Asees Kaur",
+    movie_name: "Kesari",
+    link: "https://youtu.be/j6muwUGdvXw",
+  },
+  {
+    img: "https://pagalfree.com/images/128Tum%20Ho%20-%20Rockstar%20128%20Kbps.jpg",
+    song_name: "Tum Ho​",
+    singer_name: "Mohit Chauhan, Suzanne D’Mello",
+    movie_name: "Rockstar",
+    link: "https://youtu.be/gkCKTuR-ECI",
+  },
+  {
+    img: "https://i.scdn.co/image/ab67616d0000b273ff1cda069aac9352c258f8d1",
+    song_name: "Channa Mereya​",
+    singer_name: "Arijit Singh",
+    movie_name: "Ae Dil Hai Mushkil",
+    link: "https://youtu.be/284Ov7ysmfA",
+  },
+  {
+    img: "https://i.ytimg.com/vi/ENVBeAhNz1c/maxresdefault.jpg",
+    song_name: "Hamari Adhuri Kahani​",
+    singer_name: "Arijit Singh",
+    movie_name: "Hamari Adhuri Kahani",
+    link: "https://youtu.be/f3FFOBrMmdg",
+  },
+  {
+    img: "https://i.scdn.co/image/ab67616d0000b27311dc9c9fa5d9958be9ea4084",
+    song_name: " Yeh Dooriyan​",
+    singer_name: "Mohit Chauhan",
+    movie_name: "Love Aaj Kal",
+    link: "https://youtu.be/DKOLynNhWxo",
+  },
+];
+
+export const singer = [
+  {
+    id: 1,
+    singer_name: "Atif Aslam",
+    singer_img: "",
+  },
+  {
+    id: 2,
+    singer_name: "Arijit Singh",
+    singer_img: "https://static.toiimg.com/photo/msid-94067454/94067454.jpg",
+  },
+  {
+    id: 3,
+    singer_name: "Mohit Chauhan",
+    singer_img: "",
+  },
+  {
+    id: 4,
+    singer_name: " Sonu Nigam",
+    singer_img: "",
+  },
+  {
+    id: 5,
+    singer_name: "Amit Mishra",
+    singer_img: "",
+  },
+  {
+    id: 6,
+    singer_name: "Sachet Tandon",
+    singer_img: "",
+  },
+  {
+    id: 7,
+    singer_name: "Kumar Sanu",
+    singer_img: "",
+  },
+  {
+    id: 8,
+    singer_name: "A R Rahman",
+    singer_img: "",
+  },
+  {
+    id: 9,
+    singer_name: "Shaan",
+    singer_img: "",
+  },
+  {
+    id: 10,
+    singer_name: "Sukhwinder Singh",
+    singer_img:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSTYNqsfd9lsu29FVognqRpwRGGTQL2JI2clQ&usqp=CAU",
+  },
+  {
+    id: 11,
+    singer_name: "Shankar Mahadevan",
+    singer_img:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS1iW_wVS5-0Apa4HCb2UPWRrQL8IL2gwjJ-w&usqp=CAU",
+  },
+  {
+    id: 12,
+    singer_name: "Javed Ali",
+    singer_img:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTdQotZLJXpEu58m8h2Ny980oPrFUvtZ9Z5OA&usqp=CAU",
+  },
+  {
+    id: 13,
+    singer_name: "Vishal-Shekar",
+    singer_img:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQMw9FglD2LOFbugwOB94lWhTUbvwQ4ioatCg&usqp=CAU",
+  },
+  {
+    id: 14,
+    singer_name: "KK",
+    singer_img:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRPSu_gGkl0wgBPktwKj3A5DBMhuuqZien2DQ&usqp=CAU",
+  },
+  {
+    id: 15,
+    singer_name: "Armaan Malik",
+    singer_img:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTe_0l3M-6is72KfvZcYLn7_6TDMMSfqkXuzg&usqp=CAU",
+  },
+  {
+    id: 17,
+    singer_name: "Darshan Raval",
+    singer_img:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTC3smOaDtUH8kDlE8_j4DsZ4WztTDZpftCtw&usqp=CAU",
+  },
+  {
+    id: 18,
+    singer_name: "Udit Narayan",
+    singer_img:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRTWzRRccG6yaK1JPtgAuAm__bcCyOC6kOAuA&usqp=CAU",
+  },
+  {
+    id: 19,
+    singer_name: "Jubin Nautiyal",
+    singer_img:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRk93oBGQpZBmfk8lEaPm_pAT6AHSXC8lCZw_KRHQ_J_G-HTiFZmsxsLXGrtUK9D9DM6nE&usqp=CAU",
+  },
+  {
+    id: 20,
+    singer_name: "B Praak",
+    singer_img:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRDpceJZewqqqwfFrQS7ooTigf4YbtayeiLxPk43ZnnTw&s",
+  },
+];
